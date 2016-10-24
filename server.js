e7664797755cbcd128ba1cd2f4d80e1ac7748ce8
Zhen Xie <zhen@almadendigitallabs.com>
2016-10-23 23:11:41 -0700

    Add route for web client. The web client is a Angularjs based app, and it's assumed to reside in client/web directory

--- server.js
+++ server.js
@@ -9,10 +9,34 @@ app.use(bodyParser.json());
 
 var port = 8080;
 
+function initWebClient(app) {
+    //
+    // http://stackoverflow.com/questions/20396900/angularjs-routing-in-expressjs
+    //
+    // In order to use AngularJS html5mode along with Express, you must serve "index.html" for all requests to
+    // leave all routing up to AngularJS. I had this same problem a while back.
+    // So first, you declare all API endpoint routes, any static file directories (CSS, JS, partials, etc),
+    // and then serve index.html for all remaining requests.
+    //
+    app.use('/styles', express.static(__dirname + '/client/web/styles'));
+    app.use('/scripts', express.static(__dirname + '/client/web/scripts'));
+    app.use('/assets', express.static(__dirname + '/client/web/assets'));
+    app.use('/app', express.static(__dirname + '/client/web/app'));
+    app.use('/maps', express.static(__dirname + '/client/web/maps'));
+
+    // serve index.html for all remaining routes, in order to leave routing up to angular
+    app.all("/*", function(req, res, next) {
+        res.sendfile("index.html", { root: __dirname + "/client/web" });
+    });
+}
+
 module.exports = {
     init: function() {
         logger.info('Initializing server');
         api.init(app);
+
+        logger.info('Initializing web client');
+        initWebClient(app);
     },
     getServer: function() {
         return app;

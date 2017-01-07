f4a237a3993751ee6e1716f0ac72c21192130792
Zhen Xie <zhen@almadendigitallabs.com>
2017-01-06 21:51:35 -0800

    fix script bugs

--- scripts/update_financial.sh
+++ scripts/update_financial.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-APP_DIR = $(PWD)/..
-LOGS_DIR = /tmp/logs
-TODAY = `date '+%Y_%m_%d__%H_%M_%S'`
-S3_BUCKET = s3://stock-analytics/logs
+APP_DIR=${PWD}/..
+LOGS_DIR=/tmp/logs
+TODAY=`date '+%Y_%m_%d__%H_%M_%S'`
+S3_BUCKET=s3://stock-analytics/logs
+
+# Create log folder
+mkdir -p ${LOGS_DIR}/${TODAY}
 
 # Update financial data
-node update_financials.js > $(LOGS_DIR)/$(TODAY)/update_financials.log
+node ${APP_DIR}/update_financials.js > ${LOGS_DIR}/${TODAY}/update_financials.log
 
 # Copy log file to S3
-aws s3 cp $(LOGS_DIR)/$(TODAY)/update_financials.log $(S3_BUCKET)/$(TODAY)-update_financials.log
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_financials.log ${S3_BUCKET}/${TODAY}-update_financials.log
 
 # Update EPS table
-node update_eps.js > $(LOGS_DIR)/$(TODAY)/update_eps.log
+node ${APP_DIR}/update_eps.js > ${LOGS_DIR}/${TODAY}/update_eps.log
 
 # Copy log file to S3
-aws s3 cp $(LOGS_DIR)/$(TODAY)/update_eps.log $(S3_BUCKET)/$(TODAY)-update_eps.log
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_eps.log ${S3_BUCKET}/${TODAY}-update_eps.log

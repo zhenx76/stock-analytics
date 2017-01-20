55d0a92ee55c3ca86d5ab2f639b97c8e83b24878
Zhen Xie <zhen@almadendigitallabs.com>
2017-01-19 23:20:51 -0800

    Get stock quote from Yahoo finance API

--- track_price.js
+++ track_price.js
@@ -5,6 +5,7 @@
 //
 
 var when = require('when');
+var yahooFinance = require('yahoo-finance');
 var logger = require('./utility').logger;
 var config = require('./config');
 var portfolio = require('./portfolio');
@@ -31,6 +32,57 @@ if (config.local) {
 //
 var delay = config.local ? 0 : 1000;
 
+var stockPrices = {};
+
+function getPriceSnapshot(symbol) {
+    return when.promise(function(resolve, reject) {
+        if (stockPrices.hasOwnProperty(symbol)) {
+            resolve(stockPrices[symbol]);
+        } else {
+            // Get price snapshot from Yahoo Finance
+            yahooFinance.snapshot({
+                symbol: symbol,
+                fields: ['s', 'p', 'l1']
+            }, function (err, snapshot) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (snapshot.hasOwnProperty('previousClose') && snapshot.previousClose) {
+                        stockPrices[symbol] = snapshot.previousClose;
+                        resolve(snapshot.previousClose);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function checkPriceTargets(record, price) {
+    var msg = 'Previous closing price $' + price.toFixed(2);
+    var action;
+    if (price >= record.profitPrice) {
+        msg += ' meets profit target $' + record.profitPrice.toFixed(2);
+        action = 'SELL';
+    } else if (price >= record.nextPriceTarget) {
+        msg += ' beats next price target $' + record.nextPriceTarget.toFixed(2);
+        action = 'BUY';
+    } else if (price < record.stopLossPrice) {
+        msg += ' misses stop loss price $' + record.stopLossPrice.toFixed(2);
+        action = 'SELL';
+    }
+
+    if (action) {
+        msg += '\nConsider ' + action;
+        return msg;
+    } else {
+        return null;
+    }
+}
+
+function sendEmailToUser(username, msg) {
+    logger.info(msg);
+}
+
 when.resolve()
     .then(function() { return portfolio.initPortfolioTable(db); })
     .then(function() {
@@ -38,9 +90,30 @@ when.resolve()
             return when.promise(function(resolve, reject) {
                 try {
                     logger.info('Processing ' + record.symbol + ' for ' + record.username);
-                    logger.info(JSON.stringify(record));
+
+                    when.resolve()
+                        .then(function() {
+                            return getPriceSnapshot(record.symbol);
+                        })
+                        .then(function(price) {
+                            var msg = checkPriceTargets(record, price);
+                            if (msg) {
+                                return sendEmailToUser(record.username, msg);
+                            }
+                        })
+                        .then(function() {
+                            resolve(false); // true means stop scanning the table
+                        })
+                        .catch(when.TimeoutError, function() {
+                            logger.error('Promise timeout on processing !' + record.symbol + ':' + record.username);
+                            resolve(false); // true means stop scanning the table
+                        })
+                        .catch(function(err) {
+                            logger.error("Skipping: Unable to process record.", JSON.stringify(err, null, 2));
+                            resolve(false);
+                        });
                 } catch (exception) {
-                    logger.warn(exception);
+                    logger.error(exception);
                     reject(null);
                 }
             });

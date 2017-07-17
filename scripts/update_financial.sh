005953c3be1f360e76401a9196d8dcfcc70b617c
Zhen Xie <zhenx76@gmail.com>
2017-07-16 21:27:32 -0700

    Update financial data for NYSE in the background

--- scripts/update_financial.sh
+++ scripts/update_financial.sh
@@ -8,14 +8,29 @@ S3_BUCKET=s3://stock-analytics/logs
 # Create log folder
 mkdir -p ${LOGS_DIR}/${TODAY}
 
-# Update financial data
-${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_financials.js > ${LOGS_DIR}/${TODAY}/update_financials.log
+# Update financial data for Nasdaq
+${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_financials.js -t nasdaq > ${LOGS_DIR}/${TODAY}/update_financials_nasdaq.log
 
 # Copy log file to S3
-aws s3 cp ${LOGS_DIR}/${TODAY}/update_financials.log ${S3_BUCKET}/${TODAY}-update_financials.log
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_financials_nasdaq.log ${S3_BUCKET}/${TODAY}-update_financials_nasdaq.log
 
-# Update EPS table
-${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_eps.js > ${LOGS_DIR}/${TODAY}/update_eps.log
+# Update financial data for NYSE
+${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_financials.js -t nyse > ${LOGS_DIR}/${TODAY}/update_financials_nyse.log
 
 # Copy log file to S3
-aws s3 cp ${LOGS_DIR}/${TODAY}/update_eps.log ${S3_BUCKET}/${TODAY}-update_eps.log
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_financials_nyse.log ${S3_BUCKET}/${TODAY}-update_financials_nyse.log
+
+# Update EPS table for Nasdaq
+${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_eps.js -t nasdaq > ${LOGS_DIR}/${TODAY}/update_eps_nasdaq.log
+
+# Copy log file to S3
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_eps_nasdaq.log ${S3_BUCKET}/${TODAY}-update_eps_nasdaq.log
+
+# Update EPS table for NYSE
+${HOME}/.nvm/v0.10.22/bin/node ${APP_DIR}/update_eps.js -t nyse > ${LOGS_DIR}/${TODAY}/update_eps_nyse.log
+
+# Copy log file to S3
+aws s3 cp ${LOGS_DIR}/${TODAY}/update_eps_nyse.log ${S3_BUCKET}/${TODAY}-update_eps_nyse.log
+
+# Cleanup tmp log folder
+rm -r ${LOGS_DIR}/${TODAY}

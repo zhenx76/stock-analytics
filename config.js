b682e7079f6b21cbda652ac7680e244015d72251
Zhen Xie <zhenx76@gmail.com>
2017-11-25 21:29:48 -0800

    Relace Yahoo Finance API with Alpha Vantage API.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret: 'CANSLIM',
+    alphaVantageKey: '3WBF9M8MO5DISZUE',
     local: false // Before deploy to AWS, change local to false
 };
--- price_agent.js
+++ price_agent.js
@@ -3,9 +3,71 @@
 //
 
 var when = require('when');
-var yahooFinance = require('yahoo-finance');
+var config = require('./config');
+var alpha = require('alphavantage')({key: config.alphaVantageKey});
 var logger = require('./utility').logger;
 
+function getPriceSnapshot(symbols) {
+    return when.promise(function (resolve, reject) {
+        try {
+            if (!Array.isArray(symbols)) {
+                symbols = [symbols]; // make it an array
+            }
+
+            // Query daily price using Alpha Vantage API
+            var index = 0;
+            var snapshot = {};
+
+            (function getPriceSnapshotSingle() {
+                var symbol = symbols[index];
+                alpha.data.daily(symbol).then(function(result) {
+                    var prices = [];
+                    var key = 'Time Series (Daily)';
+                    var priceKey = '4. close';
+                    if (result.hasOwnProperty(key) && result[key]) {
+                        var record = result[key];
+                        for (var dateKey in record) {
+                            if (record.hasOwnProperty(dateKey)) {
+                                var date = new Date(dateKey);
+                                if (date) {
+                                    prices.push({
+                                        date: date,
+                                        price: record[dateKey][priceKey]
+                                    })
+                                }
+                            }
+                        }
+                    }
+
+                    // sort prices array by date
+                    prices.sort(function (p1, p2) {
+                        return p2.date - p1.date;
+                    });
+
+                    snapshot[symbol] = {
+                        price: prices[0].price,
+                        change: prices[0].price - prices[1].price,
+                        changeInPercent: (prices[0].price - prices[1].price)/prices[1].price
+                    };
+
+                    if (++index == symbols.length) {
+                        resolve(snapshot);
+                    } else {
+                        setTimeout(getPriceSnapshotSingle, 0);
+                    }
+                }).catch(function(err) {
+                    reject(err);
+                });
+            })();
+        } catch (error) {
+            logger.error("Unable to download quote from Yahoo Finance.", JSON.stringify(error, null, 2));
+            resolve(null);
+        }
+    });
+}
+
+/*
+  Yahoo Finance API is no longer available
 function getPriceSnapshot(symbols) {
     return when.promise(function (resolve, reject) {
         try {
@@ -43,6 +105,7 @@ function getPriceSnapshot(symbols) {
         }
     });
 }
+ */
 
 exports.getPriceSnapshot = getPriceSnapshot;
 
@@ -53,22 +116,22 @@ var SYMBOLS = [
     'GOOG',
     'MSFT',
     'IBM',
-    'AMZN',
-    'ORCL',
-    'INTC',
-    'QCOM',
-    'FB',
-    'CSCO',
-    'SAP',
-    'TSM',
-    'BIDU',
-    'EMC',
-    'HPQ',
-    'TXN',
-    'ERIC',
-    'ASML',
-    'CAJ',
-    'YHOO'
+    'AMZN'
+    //'ORCL',
+    //'INTC',
+    //'QCOM',
+    //'FB',
+    //'CSCO',
+    //'SAP',
+    //'TSM',
+    //'BIDU',
+    //'EMC',
+    //'HPQ',
+    //'TXN',
+    //'ERIC',
+    //'ASML',
+    //'CAJ',
+    //'YHOO'
 ];
 
 getPriceSnapshot(SYMBOLS)

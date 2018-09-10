3aa07fb30749d509d7858b120dac8c43b5986ce0
Zhen Xie <zhenx76@gmail.com>
2018-09-10 10:09:20 -0700

    adding a .well-known directory support for

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ function initWebClient(app) {
     app.use('/app', express.static(__dirname + '/client/web/app'));
     app.use('/maps', express.static(__dirname + '/client/web/maps'));
 
+    // For Let's Encrypt certification
+    app.use('/.well-known', express.static(__dirname + '/client/web/well-known'));
+
     // serve index.html for all remaining routes, in order to leave routing up to angular
     app.all("/*", function(req, res, next) {
         res.sendfile("index.html", { root: __dirname + "/client/web" });
